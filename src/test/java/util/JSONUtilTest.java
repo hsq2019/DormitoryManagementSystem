d79dc6e9f26c0b4ddc68f9cfe974beab5f7e9529
hsq2019 <1484607030@qq.com>
2020-09-03 23:12:27 +0800

    更新

--- src/test/java/util/JSONUtilTest.java
+++ src/test/java/util/JSONUtilTest.java
@@ -1,12 +1,11 @@
 package util;
 
+import model.Student;
 import org.junit.Test;
 
 import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
-
-import static org.junit.Assert.*;
 
 public class JSONUtilTest {
 
@@ -21,10 +20,23 @@ public class JSONUtilTest {
     }
 
     @Test
+    public void read1() {
+        int i=1111;
+        System.out.println(JSONUtil.write(i));
+    }
+    @Test
     public void write() {
         //把j2转换成一个流，可以传入到jsonutil类里面
         InputStream is=getClass().getClassLoader().getResourceAsStream("j2.json");
         HashMap hashMap=JSONUtil.read(is,HashMap.class);
         System.out.println(hashMap);
     }
+    @Test
+    public void write1() {
+        //把j2转换成一个流，可以传入到jsonutil类里面
+        InputStream is=getClass().getClassLoader().getResourceAsStream("j2.json");
+        Student i=JSONUtil.read(is,Student.class);
+        System.out.println(i);
+    }
+
 }
